8f7769e4cce8255e42cf72946722063a740430ba
manish <manish.tiwari@infostretch.com>
2019-12-28 18:55:06 +0530

    commit data

---
+++ maven_jenkins/src/com/automation/TestAPP1.java
@@ -0,0 +1,18 @@
+package com.automation;
+
+import org.testng.annotations.Test;
+
+public class TestAPP1 {
+	
+	
+	 @Test()
+	    public void AppTest12( ) {
+	        System.out.println("working with maven project AppTest12");
+	    }
+
+	    @Test()
+	    public void AppTest13( ) {
+	        System.out.println("working with maven project AppTest13");
+	    }
+
+}
